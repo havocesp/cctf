c3c09093ca06da6b2a8f2d26fe534a51edde12d2
havocesp <umpierrez@pm.me>
2018-10-09 16:03:25 +0200

    - Code cleanup

--- cctf/__init__.py
+++ cctf/__init__.py
@@ -2,6 +2,10 @@
 """
  CCTF: Crypto-Currencies Trading Framework.
 """
+from cctf.balance import Balance, Wallet
+from cctf.core import Range, Precision, Limits
+from cctf.market import Markets, Market, Tickers, Ticker
+from cctf.symbol import Symbol, Symbols, Currency, Currencies, CURRENCIES
 
 __project__ = 'CCTF'
 __package__ = 'cctf'
@@ -13,13 +17,6 @@ __site__ = 'https://github.com/havocesp/{}'.format(__package__)
 __email__ = 'umpierrez@pm.me'
 __keywords__ = ['altcoins', 'altcoin', 'exchange', 'bitcoin', 'trading']
 
-import cctf.core
-import cctf.symbol
-from cctf.balance import Balance, Wallet
-from cctf.core import Range, Precision, Limits
-from cctf.market import Markets, Market, Tickers, Ticker
-from cctf.symbol import Symbol, Symbols, Currency, Currencies, CURRENCIES
-
 __all__ = ['__description__', '__author__', '__license__', '__version__', '__package__', '__project__', '__site__',
-           '__email__', '__keywords__', 'core', 'symbol', 'Range', 'Precision', 'Limits', 'Symbol', 'Symbols',
-           'Currency', 'Currencies', 'CURRENCIES', 'Markets', 'Market', 'Tickers', 'Ticker', 'Balance', 'Wallet']
+           '__email__', '__keywords__', 'Range', 'Precision', 'Limits', 'Symbol', 'Symbols', 'Currency', 'Currencies',
+           'CURRENCIES', 'Markets', 'Market', 'Tickers', 'Ticker', 'Balance', 'Wallet']
