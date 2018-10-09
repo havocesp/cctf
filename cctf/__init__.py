0ed955a28dd21894f086976373de422a21a6a1b2
havocesp <umpierrez@pm.me>
2018-10-09 16:30:39 +0200

    Added __deplinks__ and __dependencies__ variables containing all project dependencies.

--- cctf/__init__.py
+++ cctf/__init__.py
@@ -15,8 +15,11 @@ __version__ = '0.1.1'
 __license__ = 'MIT'
 __site__ = 'https://github.com/havocesp/{}'.format(__package__)
 __email__ = 'umpierrez@pm.me'
+__dependencies__ = list()
+__deplinks__ = ['https://github.com/havocesp/cryptocmp', 'https://github.com/havocesp/logging4humans']
 __keywords__ = ['altcoins', 'altcoin', 'exchange', 'bitcoin', 'trading']
 
 __all__ = ['__description__', '__author__', '__license__', '__version__', '__package__', '__project__', '__site__',
            '__email__', '__keywords__', 'Range', 'Precision', 'Limits', 'Symbol', 'Symbols', 'Currency', 'Currencies',
-           'CURRENCIES', 'Markets', 'Market', 'Tickers', 'Ticker', 'Balance', 'Wallet']
+           'CURRENCIES', 'Markets', 'Market', 'Tickers', 'Ticker', 'Balance', 'Wallet', '__deplinks__',
+           '__dependencies__']
